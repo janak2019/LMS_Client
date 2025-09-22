c7842b7c66ea5a12a92a15c6ffcb4ec40c33d811
htmlbasic <withjanak@gmail.com>
2025-09-22 19:12:00 +0545

    backend url update

--- src/store/slices/bookSlice.js
+++ src/store/slices/bookSlice.js
@@ -47,7 +47,7 @@ const bookSlice = createSlice({
 
 export const fetchAllBooks = ()=>async(dispatch)=>{
     dispatch(bookSlice.actions.fetchBooksRequest())
-    await axios.get("http://localhost:4000/api/v1/book/all",{withCredentials:true})
+    await axios.get("https://lms-server-73ra.onrender.com/api/v1/book/all",{withCredentials:true})
     .then(res=>{
         dispatch(bookSlice.actions.fetchBooksSuccess(res.data.books))
     })
@@ -57,7 +57,7 @@ export const fetchAllBooks = ()=>async(dispatch)=>{
 }
 export const addBook = (data) =>async(dispatch)=>{
     dispatch(bookSlice.actions.addBookRequest())
-    await axios.post("http://localhost:4000/api/v1/book/admin/add",data,{
+    await axios.post("https://lms-server-73ra.onrender.com/api/v1/book/admin/add",data,{
         withCredentials: true,
         headers:{
             "Content-Type":"application/json"
